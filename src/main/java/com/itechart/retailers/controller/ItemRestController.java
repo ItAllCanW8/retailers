814c1f97c50080e774f0acda06043488b9451106
Mikhail Leonovets <mikhail.leonovets@gmail.com>
2021-11-26 15:09:09 +0300

    merge item-service

--- src/main/java/com/itechart/retailers/controller/ItemRestController.java
+++ src/main/java/com/itechart/retailers/controller/ItemRestController.java
@@ -1,44 +1,44 @@
 package com.itechart.retailers.controller;
 
-//import com.itechart.retailers.model.Item;
+import com.itechart.retailers.model.Item;
+import com.itechart.retailers.service.ItemService;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-//@RestController
-//@RequestMapping("/items")
-//@CrossOrigin("*")
-//public class ItemRestController {
-//    private List<Item> items = List.of(
-//            new Item(1L, "Manka"),
-//            new Item(2L, "Grechka"),
-//            new Item(3L, "Perlovka")
-//    );
-//
-//    @GetMapping
-//    public List<Item> getAll() {
-//        return items;
-//    }
-//
-//    @GetMapping("/{id}")
-//    @PreAuthorize("hasAuthority('item:read')")
-//    public Item getById(@PathVariable Long id) {
-//        return items.stream().filter(item -> item.getId().equals(id))
-//                .findFirst()
-//                .orElse(null);
-//    }
-//
-//    @PostMapping
-//    @PreAuthorize("hasAuthority('item:write')")
-//    public Item create(@RequestBody Item item) {
-//        this.items.add(item);
-//        return item;
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    @PreAuthorize("hasAuthority('item:write')")
-//    public void deleteById(@PathVariable Long id) {
-//        this.items.removeIf(item -> item.getId().equals(id));
-//    }
-//}
+@RestController
+@RequestMapping("/items")
+@CrossOrigin("*")
+public class ItemRestController {
+
+	private final ItemService itemService;
+
+	public ItemRestController(ItemService itemService) {
+		this.itemService = itemService;
+	}
+
+	@GetMapping
+	public List<Item> getAll() {
+		return itemService.findAll();
+	}
+
+	@GetMapping("/{id}")
+	@PreAuthorize("hasAuthority('item:read')")
+	public Item getById(@PathVariable Long id) {
+		return itemService.getById(id);
+	}
+
+	@PostMapping
+	@PreAuthorize("hasAuthority('item:write')")
+	public Item create(@RequestBody Item item) {
+		itemService.save(item);
+		return item;
+	}
+
+	@DeleteMapping("/{id}")
+	@PreAuthorize("hasAuthority('item:write')")
+	public void deleteById(@PathVariable Long id) {
+		itemService.deleteById(id);
+	}
+}
