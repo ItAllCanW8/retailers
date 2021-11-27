57dc7c27bc707f79f2ecd98ed5afc495450181f7
itallcanw8 <mironchik.a@gmail.com>
2021-11-27 12:46:53 +0300

    Category fix

--- src/main/java/com/itechart/retailers/model/Category.java
+++ src/main/java/com/itechart/retailers/model/Category.java
@@ -13,11 +13,6 @@ import javax.persistence.*;
 @Getter
 @Setter
 public class Category extends Identity {
-
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Integer id;
-
     @Column(name = "name", nullable = false, length = 45)
     private String name;
 }
