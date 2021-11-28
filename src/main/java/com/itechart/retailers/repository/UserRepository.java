b4aec153b82ae1d55e577a63873c670a093da1ff
Mikhail Leonovets <mikhail.leonovets@gmail.com>
2021-11-28 10:46:31 +0300

    admin controller created

--- src/main/java/com/itechart/retailers/repository/UserRepository.java
+++ src/main/java/com/itechart/retailers/repository/UserRepository.java
@@ -1,15 +1,19 @@
 package com.itechart.retailers.repository;
 
+import com.itechart.retailers.model.Role;
 import com.itechart.retailers.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByEmail(String email);
+	Optional<User> findByEmail(String email);
 
-    Boolean existsByEmail(String email);
+	Boolean existsByEmail(String email);
+
+	List<User> findUserByRole(Role role);
 }
