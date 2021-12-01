5d00870a6fab12cfb4ca33d8db1ceb6a456d464b
itallcanw8 <mironchik.a@gmail.com>
2021-12-01 12:37:12 +0300

    Changed SystemAdmin request mapping

--- src/main/java/com/itechart/retailers/controller/SystemAdminController.java
+++ src/main/java/com/itechart/retailers/controller/SystemAdminController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/system_admin")
+@RequestMapping("/api/system-admin")
 @RequiredArgsConstructor
 public class SystemAdminController {
 
