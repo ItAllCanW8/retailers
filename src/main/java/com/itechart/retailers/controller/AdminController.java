12d305ce6d8ad7b7b1f99b63d765951c78c18b3f
itallcanw8 <mironchik.a@gmail.com>
2021-12-01 14:35:05 +0300

    AdminController started (loading customer locations with tests)

---
+++ src/main/java/com/itechart/retailers/controller/AdminController.java
@@ -0,0 +1,40 @@
+package com.itechart.retailers.controller;
+
+import com.itechart.retailers.model.entity.Location;
+import com.itechart.retailers.service.AdminService;
+import com.itechart.retailers.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/admin")
+public class AdminController {
+    private final AdminService adminService;
+    private final UserService userService;
+    private final String authorities = "hasAuthority('ADMIN')";
+
+    @GetMapping("/locations")
+    @PreAuthorize(authorities)
+    public List<Location> getLocations(Authentication authentication) {
+        String adminEmail = authentication.getName();
+
+//        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//
+//        if (principal instanceof UserDetails) {
+//            String email = ((UserDetails)principal).getUsername();
+//        } else {
+//            String username = principal.toString();
+//        }
+
+        return adminService.findLocations(adminEmail);
+    }
+}
--- src/main/java/com/itechart/retailers/model/entity/ApplicationItem.java
+++ src/main/java/com/itechart/retailers/model/entity/ApplicationItem.java
@@ -6,6 +6,7 @@ import javax.persistence.*;
 
 @Getter
 @Setter
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
