b3d2c43717f7e024a4c64922c277dbc285ce1887
Vit <vitalisnitko228@gmail.com>
2021-12-23 02:27:29 +0300

    .

--- react-front/src/component/users/UsersInnerModal.js
+++ react-front/src/component/users/UsersInnerModal.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { Autocomplete, TextField } from '@mui/material';
 
 export class UsersInnerModal extends Component {
