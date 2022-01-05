cd4167c10ccdb4f39f702be5e0b543527dc67474
itallcanw8 <mironchik.a@gmail.com>
2022-01-05 16:37:54 +0300

    CustomerCategoryRepositoryTest

---
+++ src/test/java/com/itechart/retailers/repository/CustomerCategoryRepositoryTest.java
@@ -0,0 +1,58 @@
+package com.itechart.retailers.repository;
+
+import com.itechart.retailers.model.entity.Category;
+import com.itechart.retailers.model.entity.Customer;
+import com.itechart.retailers.model.entity.CustomerCategory;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DataJpaTest
+class CustomerCategoryRepositoryTest {
+
+    @Autowired
+    private CustomerCategoryRepository underTest;
+    @Autowired
+    private CustomerRepository customerRepository;
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    void shouldUpdateItemCategoryTax() {
+        //given
+        float newTax = 0.05f;
+
+        Customer customer = Customer.builder()
+                .name("Customer")
+                .regDate(LocalDate.now())
+                .isActive(true)
+                .build();
+        customer = customerRepository.save(customer);
+
+        Category category = Category.builder()
+                .name("Bread")
+                .build();
+        category = categoryRepository.save(category);
+//
+        CustomerCategory customerCategory = CustomerCategory.builder()
+                .category(category)
+                .customer(customer)
+                .categoryTax(0.0f)
+                .build();
+        customerCategory = underTest.save(customerCategory);
+        //when
+        int result = underTest.updateItemCategoryTax(customerCategory.getId(), newTax);
+        //then
+        assertThat(result).isEqualTo(1);
+
+        System.out.println(underTest.findByCustomerIdAndCategoryId(customer.getId(), category.getId()).get().getCategoryTax());
+
+//        assertThat(underTest.findById(customerCategory.getId()).get().getCategoryTax()).isEqualTo(newTax);
+    }
+}
